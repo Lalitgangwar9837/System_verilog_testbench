0a13202264860042bcde6842cd4e0349a50c1c24
Lalitgangwar9837 <81949971+Lalitgangwar9837@users.noreply.github.com>
2022-12-27 01:59:50 +0530

    Create enviroment.sv
    full_adder

---
+++ enviroment.sv
@@ -0,0 +1,31 @@
+class environment;
+  generator gen;
+  driver dri;
+  scoreboard sco;
+  monitor mon;
+  mailbox m1;
+  mailbox m2;
+  
+  virtual intf i3;
+  function new (virtual intf i3);
+    this.i3=i3 ;
+  endfunction
+  
+  task run();
+    m1=new(1);
+    m2=new(1);
+    gen=new(m1);
+    dri=new(i3,m1);
+    sco=new(m2);
+    mon=new(i3,m2);
+    repeat (15)
+      fork
+      gen.main();
+      dri.main();
+      mon.main();  
+      sco.main();
+      
+        
+      join
+  endtask
+endclass
