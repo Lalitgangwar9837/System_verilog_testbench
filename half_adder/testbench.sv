d72c266978c73d230fab14b4bde1c57a0189ce41
Lalitgangwar9837 <81949971+Lalitgangwar9837@users.noreply.github.com>
2022-12-28 23:33:52 +0530

    Update testbench.sv

--- half_adder/testbench.sv
+++ half_adder/testbench.sv
@@ -1,1 +1,18 @@
+`include "design.sv"
+`include "interface.sv"
 
+`include "test.sv"
+
+module tbench_top;
+  
+  intf i_intf();
+  
+  test t1 (i_intf);
+  
+  half_adder h1(.a(i_intf.a),.b(i_intf.b),.sum(i_intf.sum),.carry(i_intf.carry));
+  initial
+    begin
+      $dumpfile("dum.vcd");
+      $dumpvars;
+    end
+endmodule
