f590fa5b78867506654200c73886c0df59132bae
Lalitgangwar9837 <81949971+Lalitgangwar9837@users.noreply.github.com>
2022-12-28 23:39:54 +0530

    Create driver.sv

---
+++ half_adder/driver.sv
@@ -0,0 +1,30 @@
+class driver;
+  
+  virtual intf vif;
+  
+  mailbox gen2driv;
+  
+  function new (virtual intf vif,mailbox gen2driv);
+    this.vif=vif;
+    this.gen2driv = gen2driv;
+  endfunction
+  task main;
+    
+    repeat (1)
+      begin
+        
+        transaction trans;
+        
+        
+        gen2driv.get(trans);
+        
+        vif.a <= trans.a;
+        vif.b <= trans.b;
+        
+        trans.sum  = vif.sum;
+        trans.carry = vif.carry;
+        trans.display("driver");
+      end
+  endtask
+  
+endclass
