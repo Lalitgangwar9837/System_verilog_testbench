57a9095f78ef17b4cb16380328f45b79b7c372d0
Lalitgangwar9837 <81949971+Lalitgangwar9837@users.noreply.github.com>
2022-12-28 23:38:10 +0530

    Create monitor.sv

---
+++ half_adder/monitor.sv
@@ -0,0 +1,29 @@
+class monitor;
+  
+  virtual intf vif;
+  
+  mailbox mon2scb;
+  
+  function new ( virtual intf vif,mailbox mon2scb);
+    this.vif =vif;
+    this.mon2scb =mon2scb;
+  endfunction
+  
+  task main;
+    repeat (1)
+      #3;
+    begin
+      transaction trans;
+      trans=new();
+      trans.a =vif.a;
+      trans.b = vif.b;
+      trans.sum =vif.sum;
+      trans.carry = vif.carry;
+      mon2scb.put(trans);
+     // trans.display("monitor");
+      
+    end
+  endtask
+  
+endclass
+                    
