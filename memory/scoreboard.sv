973d4b01aa8453251032d78be7e75dbfde2ad0d8
Lalitgangwar9837 <81949971+Lalitgangwar9837@users.noreply.github.com>
2023-02-02 18:45:48 +0530

    Update scoreboard.sv

--- memory/scoreboard.sv
+++ memory/scoreboard.sv
@@ -1,19 +1,19 @@
 class scoreboard;
 bit [7:0] ref_w_data;
 bit [7:0] ref_r_data;
-mailbox m9;
-mailbox m8;
+mailbox mon_to_soc;
+mailbox ref_to_soc;
 virtual bus b10;
-function new (mailbox m8,mailbox m9);
-	this.m8=m8;
-	this.m9=m9;
+function new (mailbox ref_to_soc,mailbox mon_to_soc);
+	this.ref_to_soc=ref_to_soc;
+	this.mon_to_soc=mon_to_soc;
 endfunction
 task main;
 packet pac=new();
 
-m8.get(ref_r_data);
-m8.get(ref_w_data);
-m9.get(pac);
+ref_to_soc.get(ref_r_data);
+ref_to_soc.get(ref_w_data);
+mon_to_soc.get(pac);
 if ((ref_r_data==pac.r_data)&&(ref_w_data==pac.w_data))
 	begin
 	 $display("----/////////////////////////////////-------------"); 
